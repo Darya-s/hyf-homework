b75440b85b9837bae5a864e3dee542e6d881fba9
Darya-s <darya.stepanova95@gmail.com>
2021-06-30 22:39:51 +0200

    Commmited exercise 3,a house price estimator

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -12,6 +12,21 @@ function volumeInMeters(width,height,depth){
 
 var petersHouse=(volumeInMeters(8,10,10)*2.5*1000 + gardenSizeInM2(100)*300);
 
-console.log(petersHouse);
+if(petersHouse>2500000){
+    console.log("Peter is paying too much!")
+}
+else if(petersHouse<2500000){
+    console.log("Peter is paying too little")
+}
 
 var juliasHouse=(volumeInMeters(5,8,11)*2.5*1000 + gardenSizeInM2(70)*300);
+
+if(juliasHouse>2500000){
+    console.log("Julia is paying too much!")
+}
+else if(juliasHouse<2500000){
+    console.log("Julia is paying too little")
+}
+
+
+
