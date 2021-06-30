bbddecc3d699e92f2b9659b1402f7403efc50508
Darya-s <darya.stepanova95@gmail.com>
2021-06-30 22:47:36 +0200

    modified exercise 1, age calculator

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,4 +1,7 @@
-var yearOfBirth;
-var yearFuture;
+var yearOfBirth=1995;
+var yearFuture=2027;
 
 var age=yearFuture-yearOfBirth;
+
+console.log ("You will be"+ age +" years old in "+ yearFuture+".");
+
