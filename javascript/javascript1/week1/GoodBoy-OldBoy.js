a7d53229505a2fcf846fd2889ca6598d2a82c0d6
Darya-s <darya.stepanova95@gmail.com>
2021-06-30 22:48:06 +0200

    modified exercise2, dog age claculator

---
+++ javascript/javascript1/week1/GoodBoy-OldBoy.js
@@ -0,0 +1,11 @@
+var dogYearOfBirth=2021;
+var dogYearFuture=2045;
+var dogYear=dogYearFuture-dogYearOfBirth;
+var shouldShowResultInDogYears=true;
+
+if(shouldShowResultInDogYears){
+console.log("Your dog will be"+ dogYear + "+human years old in"+2027);
+}
+else{
+    console.log("Your dog will be"+ dogYear*10+ "+human years old in"+2027);
+}
