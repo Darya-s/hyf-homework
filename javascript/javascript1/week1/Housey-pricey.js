370c98e470cabb59355bb9039c1237a639a7f654
Darya-s <darya.stepanova95@gmail.com>
2021-07-02 20:18:13 +0200

    Changed var to let and const variables

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -1,7 +1,5 @@
 
 
-
-
 function volumeInMeters(width,height,depth){
     return width*height*depth;
 };
@@ -16,7 +14,7 @@ if(petersHouse>2500000){
     console.log("Peter is paying too much!")
 }
 else if(petersHouse<2500000){
-    console.log("Peter is paying too little")
+    console.log("Peter is paying too little!")
 }
 
 let juliasHouse=(volumeInMeters(5,8,11)*2.5*1000 + gardenSizeInM2(70)*300);
