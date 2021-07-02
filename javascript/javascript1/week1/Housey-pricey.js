291c717ded826dff155e00c66a42e2180e3ca276
Darya-s <darya.stepanova95@gmail.com>
2021-07-02 22:21:47 +0200

    changed code in exercise3,house price. Seems it works

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -1,30 +1,24 @@
-
-
-function volumeInMeters(width,height,depth){
-    return width*height*depth;
-};
-
- function gardenSizeInM2(gardenSize){
-     Math.pow(gardenSize , 2);}
-
-
-let petersHouse=(volumeInMeters(8,10,10)*2.5*1000 + gardenSizeInM2(100)*300);
-
-if(petersHouse>2500000){
-    console.log("Peter is paying too much!")
-}
-else if(petersHouse<2500000){
-    console.log("Peter is paying too little!")
+function volumeInMeters(width, height, depth) {
+  return width * height * depth;
 }
 
-let juliasHouse=(volumeInMeters(5,8,11)*2.5*1000 + gardenSizeInM2(70)*300);
+let volPeter = volumeInMeters(8, 10, 10) * 2.5 * 1000;
+let gardenPeter = Math.pow(100, 2) * 300;
+let petersHouse = volPeter + gardenPeter;
 
-if(juliasHouse>2500000){
-    console.log("Julia is paying too much!")
+if (petersHouse > 2500000) {
+  console.log("Peter is paying too much!");
+} else if (petersHouse < 2500000) {
+  console.log("Peter is paying too little!");
 }
-else if(juliasHouse<2500000){
-    console.log("Julia is paying too little")
+
+let volJulia = volumeInMeters(5, 8, 11) * 2.5 * 1000;
+let gardenJulia = Math.pow(70, 2) * 300;
+
+let juliasHouse = volJulia + gardenJulia;
+
+if (juliasHouse > 2500000) {
+  console.log("Julia is paying too much!");
+} else if (juliasHouse < 2500000) {
+  console.log("Julia is paying too little");
 }
-
-
-
