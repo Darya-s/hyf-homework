e64def7cf9e5663cade3bdea6da1aa1cdb2e8235
Darya-s <darya.stepanova95@gmail.com>
2021-07-02 11:50:27 +0200

    Added exercise 2,dog age calculator

--- javascript/javascript1/week1/GoodBoy-OldBoy.js
+++ javascript/javascript1/week1/GoodBoy-OldBoy.js
@@ -1,7 +1,7 @@
-var dogYearOfBirth=2021;
-var dogYearFuture=2045;
-var dogYear=dogYearFuture-dogYearOfBirth;
-var shouldShowResultInDogYears=true;
+const dogYearOfBirth=2021;
+const dogYearFuture=2045;
+let dogYear=dogYearFuture-dogYearOfBirth;
+let shouldShowResultInDogYears=true;
 
 if(shouldShowResultInDogYears){
 console.log("Your dog will be"+ dogYear + "+human years old in"+2027);
