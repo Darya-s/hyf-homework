32d72fc56dd387320120c1dc67636539397245e2
Darya-s <darya.stepanova95@gmail.com>
2021-07-02 11:49:42 +0200

    Changed var to const in age calculator ,exercise 1

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,7 +1,7 @@
-var yearOfBirth=1995;
-var yearFuture=2027;
+const yearOfBirth=1995;
+const yearFuture=2027;
 
-var age=yearFuture-yearOfBirth;
+let age=yearFuture-yearOfBirth;
 
 console.log ("You will be"+ age +" years old in "+ yearFuture+".");
 
