70aa925e0ec28cd2348a11ea217d07a5c805206e
Darya-s <darya.stepanova95@gmail.com>
2021-07-03 01:03:34 +0200

    changed let to const in exercise 3

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -2,9 +2,9 @@ function volumeInMeters(width, height, depth) {
   return width * height * depth;
 }
 
-let volPeter = volumeInMeters(8, 10, 10) * 2.5 * 1000;
-let gardenPeter = Math.pow(100, 2) * 300;
-let petersHouse = volPeter + gardenPeter;
+const volPeter = volumeInMeters(8, 10, 10) * 2.5 * 1000;
+const gardenPeter = Math.pow(100, 2) * 300;
+const petersHouse = volPeter + gardenPeter;
 
 if (petersHouse > 2500000) {
   console.log("Peter is paying too much!");
@@ -12,10 +12,10 @@ if (petersHouse > 2500000) {
   console.log("Peter is paying too little!");
 }
 
-let volJulia = volumeInMeters(5, 8, 11) * 2.5 * 1000;
-let gardenJulia = Math.pow(70, 2) * 300;
+const volJulia = volumeInMeters(5, 8, 11) * 2.5 * 1000;
+const gardenJulia = Math.pow(70, 2) * 300;
 
-let juliasHouse = volJulia + gardenJulia;
+const juliasHouse = volJulia + gardenJulia;
 
 if (juliasHouse > 2500000) {
   console.log("Julia is paying too much!");
