9aaa185f867d1046f8bf639b95b9b0ef06f20b70
Darya-s <darya.stepanova95@gmail.com>
2021-07-03 01:02:54 +0200

    changed let to const in exercise 2 ,as requested

--- javascript/javascript1/week1/GoodBoy-OldBoy.js
+++ javascript/javascript1/week1/GoodBoy-OldBoy.js
@@ -1,11 +1,10 @@
-let dogYearOfBirth=2021;
-let dogYearFuture=2045;
-let dogYear=dogYearFuture-dogYearOfBirth;
-let shouldShowResultInDogYears=true;
+const dogYearOfBirth = 2021;
+const dogYearFuture = 2045;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
 
-if(shouldShowResultInDogYears){
-console.log("Your dog will be"+ dogYear + "+human years old in"+2027);
-}
-else{
-    console.log("Your dog will be"+ dogYear*10+ "+human years old in"+2027);
+if (shouldShowResultInDogYears) {
+  console.log("Your dog will be" + dogYear + "+human years old in" + 2027);
+} else {
+  console.log("Your dog will be" + dogYear * 10 + "+human years old in" + 2027);
 }
