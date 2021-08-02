da69fb714b0a88166c992ab7420657fad569b76d
Darya-s <darya.stepanova95@gmail.com>
2021-08-02 16:43:22 +0200

    Fixed smart-ease notes exercise

--- javascript/javascript1/week3/smart-ease.js
+++ javascript/javascript1/week3/smart-ease.js
@@ -13,18 +13,15 @@ const notepad = [
 ];
 
 function saveNote(content, id) {
-  // write some code her
   for (let note of notepad) {
-    notes.push(note);
+    notes.push({ content, id });
   }
   return notes;
 }
 saveNote(notepad);
 console.log(notes);
-//saveNote("Pick up groceries", 1);
-//saveNote("Do laundry", 2);
+
 function getNote(id) {
-  // your code here
   for (let i = 0; i < notes.length; i++) {
     if (notes[i].id == id) {
       return notes[i].content;
@@ -32,11 +29,11 @@ function getNote(id) {
   }
 }
 
-const firstNote = getNote(2);
-console.log(firstNote); // {content: 'Pick up groceries', id: 1}
+const firstNote = getNote(1);
+const secondNote = getNote(2);
+console.log(firstNote, secondNote);
 
 function logOutNotesFormatted() {
-  // your code here
   for (let i = 0; i < notes.length; i++) {
     console.log(
       "The note with id: " +
@@ -47,7 +44,7 @@ function logOutNotesFormatted() {
   }
 }
 
-logOutNotesFormatted(); // should log out the text below
+logOutNotesFormatted();
 
 function addNote(newContent, newId) {
   notepad.push({ id: newId, content: newContent });
@@ -55,6 +52,3 @@ function addNote(newContent, newId) {
 
 addNote("My new note number three", 3);
 console.log(notepad);
-
-// The note with id: 1, has the following note text: Pick up groceries
-// The note with id: 2, has the following note text: Do laundry
