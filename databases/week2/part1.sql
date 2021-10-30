b6fff7e7538baaf4e21a4da197ef4cc362ba87c3
Darya-s <darya.stepanova95@gmail.com>
2021-10-30 13:36:56 +0200

    Added dbweek2 part1 implemented after feedback

--- databases/week2/part1.sql
+++ databases/week2/part1.sql
@@ -1,15 +1,14 @@
 
-ALTER TABLE task
-ADD COLUMN user_id VARCHAR(15) AFTER id;
+insert into task (id, title, description, created, updated, due_date, status_id) values (36, 'Wash cars', 'Todays task.', '2017-09-21 06:54:16', '2017-10-15 13:05:09', null, 1);
 
 
 UPDATE task
 SET title = 'Alterated title'
-WHERE id>=34;
+WHERE id=36;
 
 UPDATE task
 SET due_date='2021-10-10 14:57:22'
-WHERE due_date='2017-10-10 14:57:22';
+WHERE id=36;
 
 UPDATE task
 SET status_id = 3
